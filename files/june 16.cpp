3f3dd53134ea3171013f99e0a7231cfe4dc0cf1f
Leejaw-Chitrakar <leejaw.chitrakar10@gmail.com>
2025-06-16 08:53:51 +0545

    Static Data Member and Static Member Function

---
+++ files/june 16.cpp
@@ -0,0 +1,98 @@
+// WAP to count total objects created for a class
+// Static Date Member Function
+/*
+#include <iostream>
+using namespace std;
+
+class car {
+    private:
+        string model, make;
+        int year;
+        static int count;
+    public:
+        void set_date(string m, string mk, int y) {
+            model = m;
+            make = mk;
+            year = y;
+            count++;
+        }
+
+        void display() {
+            cout << "Model: " << model << endl;
+            cout << "Make: " << make << endl;
+            cout << "Year: " << year << endl;
+            cout << "Total cars created: " << count << endl;
+            cout << "*********************" << endl;
+        }
+};
+
+int car::count = 0;
+
+int main(){
+    string model, make;
+    int year;
+    car c1;
+    cout << "-------------------------" << endl;
+    cout << "Date of car 1: " << endl;
+    cout << "-------------------------" << endl;
+    c1.set_date("ModelX", "Tesla", 2020);
+    c1.display();
+    cout << endl ;
+    car c2;
+    cout << "-------------------------" << endl;
+    cout << "Date of car 2: " << endl;
+    cout << "-------------------------" << endl;
+
+    c2.set_date("ModelS", "Tesla", 2021);
+    c2.display();
+
+    return 0;
+}
+*/
+
+
+/* Static Members Function*/
+/*
+#include <iostream>
+using namespace std;
+
+class book{
+    private:
+        string name;
+        float price;
+        static int count;
+    public:
+    void setData(string n, float p) {
+        name = n;
+        price = p;
+        count++;
+    }
+
+    void displayData() {
+        cout << "Book Name  : " << name << endl;
+        cout << "Price(NRs.): " << price << endl << endl;
+    }
+
+    static void display() {
+        cout << "Total books created: " << count << endl;
+    }
+
+    static int getCount() {
+        return count;
+    }
+};
+
+int book::count = 0;
+
+int main(){
+    book b1, b2, b3;
+    b1.setData("Math", 250.0);
+    b2.setData("Science", 300.0);
+    b3.setData("History", 200.0);
+    b1.displayData();
+    b2.displayData();
+    b3.displayData();
+    book::display();
+    return 0;
+}
+*/
