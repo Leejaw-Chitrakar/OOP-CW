458a2c8006aac9bb587214631c369b6aa13f946a
Leejaw-Chitrakar <leejaw.chitrakar10@gmail.com>
2025-06-17 07:26:44 +0545

    june 17

--- files/june 16.cpp
+++ files/june 16.cpp
@@ -50,7 +50,6 @@ int main(){
 }
 */
 
-
 /* Static Members Function*/
 /*
 #include <iostream>
@@ -97,9 +96,8 @@ int main(){
 }
 */
 
-
 /* Area of Retangle */
-
+/*
 #include <iostream>
 using namespace std;
 
@@ -119,9 +117,9 @@ class rect{
         cout << "*********************" << endl;
     }
 
-    void add(rect r){
-        length += r.length;
-        breadth += r.breadth;
+    void add(rect r6, rect r7){
+        length = r6.length + r7.length;
+        breadth = r6.breadth + r7.breadth;
     }
 
     void display_sum_area() {
@@ -133,14 +131,102 @@ class rect{
 };
 
 int main(){
-    rect r1, r2;
+    rect r1, r2, r3;
     int l, b;
     r1.set_data(10, 5);
     r1.display();
     r2.set_data(20, 10);
     r2.display();
-    r1.add(r2);
+    r3.add(r1,r2);
     cout << "After adding r1 and r2:" << endl;
     r1.display_sum_area();
     return 0;
+}
+*/
+
+
+/* June 17 */
+
+
+/* Return object form functions */
+/*
+#include <iostream>
+using namespace std;
+
+class rect
+{
+private:
+    int length, breadth;
+public:
+    void set_data(int l, int b){
+        length = l;
+        breadth = b;
+    }
+
+    void display(){
+        cout << "Length = " << length << endl;
+        cout << "Breadth= " << length << endl;
+        cout << "Area = " << length*breadth << endl;
+    }
+
+    rect add(rect r6){
+        rect r8;
+        r8.length = length + r6.length;
+        r8.breadth = breadth + r6.breadth;
+        return r8;
+    }
+};
+
+int main()
+{
+    rect r1, r2, r3;
+    r1.set_data(12, 5);
+    r2.set_data(10, 7);
+    r1.display();
+    r2.display();
+    r3 = r1.add(r2);
+    r3.display();
+    return 0;
+}
+*/
+
+/* WAP to create a class colmpex having attributes real and imaginary.
+    create two objects c1 and c2 abd assign the vlaues to attrubutes.
+    create a c3 object by addding c1 and c2.
+*/
+
+#include <iostream>
+using namespace std;
+
+class complex{
+private:
+    float real, imiginary;
+public:
+    void setdata(float r, float i){
+        real = r;
+        imiginary = i;
+    }
+
+    void display(){
+        cout << "Real = " << real << endl;
+        cout << "Imiginary = " << imiginary << endl;
+        cout << "Sum = " << real << " + " << imiginary << "i" << endl;
+
+    }
+
+    complex addcomplex(complex c){
+        complex a;
+        a.real = real + c.real;
+        a.imiginary = imiginary + c.imiginary;
+        return a;
+    }
+};
+
+int main(){
+    complex c1, c2 ,c3;
+    c1.setdata(3, 5);
+    c2.setdata(4, 6);
+
+    c3 = c2.addcomplex(c1);
+    c3.display();
 }
