19dfd8bac2b6cb68364a71d791c5d8780ce01d20
Leejaw-Chitrakar <leejaw.chitrakar10@gmail.com>
2025-06-20 09:05:10 +0545

    Nameless Temporary Argument

--- files/june 20.cpp
+++ files/june 20.cpp
@@ -107,3 +107,55 @@ int main()
 
 
 
+/* Nameless Temporary Argument */
+/* Its called nameless because only the compiler uses it not the used and the process is hidded */
+/* Always use parameterized constructor */
+/* Also include a Default constructor */
+
+/*
+#include <iostream>
+using namespace std;
+
+class car{
+    private:
+    int price;
+    public:
+    car(){
+        price = 150000;
+    }
+
+    void display(){
+        cout << "Price os the car is Rs."<< price << endl;
+    }
+
+    void setdata(int p){
+        price = p;
+    }
+
+    car operator ++(){
+        car temp;
+        temp.price = ++price;
+        return temp;
+    }
+
+    // Parameterized Constructor
+    car(int p){
+        price = p;
+    }
+    
+};
+
+int main(){
+    car c1,c2,c3;
+    c1.display();
+    c2 = ++c1;
+    c2.display();
+    c3.setdata(250000);
+    c3.display();
+    c2 = ++c3;
+    c2.display();
+    return 0;
+}
+*/
+
+
