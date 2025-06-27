258b61d034864be569a635c863295833b8c2a3e8
Leejaw-Chitrakar <leejaw.chitrakar10@gmail.com>
2025-06-27 08:28:13 +0545

    Lab 1 and 2 completed

--- LAB/Lab-2/Inline functions.cpp
+++ LAB/Lab-2/Inline functions.cpp
@@ -8,12 +8,12 @@ inline int mul(int n1, int n2){
 int main(){
     int num1,num2,res;
     num1 = 4;
-    num2 = 5;
+    num2 = 8;
     res = mul(num1,num2);
     cout << "Result of multiplying " << num1 << " and " << num2 << ": " << res << endl;
 
-    num1 = 8;
-    num2 = 4;
+    num1 = 6;
+    num2 = 6;
     res = mul(num1,num2);
     cout << "Result of multiplying " << num1 << " and " << num2 << ": " << res << endl;
 
