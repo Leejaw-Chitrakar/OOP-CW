73ceaad3249c4c5a0421057a48386464964bb0ea
Leejaw-Chitrakar <leejaw.chitrakar10@gmail.com>
2025-07-01 20:31:32 +0545

    code corrected

--- files/Conversion NRS to USD.cpp
+++ files/Conversion NRS to USD.cpp
@@ -1,1 +1,100 @@
 // Assignment: Convert NRS to USD
+#include <iostream>
+using namespace std;
+
+class NRS;
+class USD;
+
+class NRS
+{
+private:
+    int rs, ps;
+
+public:
+    NRS()
+    {
+        rs = 0;
+        ps = 0;
+    }
+
+    NRS(int r, int p)
+    {
+        rs = r;
+        ps = p;
+    }
+
+    void display()
+    {
+        cout << "---------------------------------------" << endl;
+        cout << "Rupee: " << rs << endl
+             << "Paisa: " << ps << endl;
+        cout << "---------------------------------------" << endl;
+    }
+    int getrs()
+    {
+        return rs;
+    }
+
+    int getps()
+    {
+        return ps;
+    }
+};
+
+class USD
+{
+private:
+    int usd, ce;
+
+public:
+    USD()
+    {
+        usd = 0;
+        ce = 0;
+    }
+
+    USD(int u, int c)
+    {
+        usd = u;
+        ce = c;
+    }
+
+    void display()
+    {
+        cout << "---------------------------------------" << endl;
+        cout << "USD: " << usd << endl
+             << "Cent: " << ce << endl;
+        cout << "---------------------------------------" << endl;
+    }
+
+    USD(NRS n3)
+    {
+        // Conversion from NRS to USD
+        // 1 USD = 137.33 NRS
+        usd = n3.getrs() / 137.33;
+        ce = n3.getps() / 137.33 * 100; // Convert paisa to cent
+        usd = static_cast<int>(usd);
+        ce = static_cast<int>(ce + 0.5); // rounding to nearest cent
+    }
+
+    // operator NRS()
+    // {
+    //     // 1 usd = 137.33 rs
+    //     float total_nrs = usd * 137.33 + ce * 137.33 / 100.0;
+    //     int rupees = static_cast<int>(total_nrs);
+    //     int paisa = static_cast<int>((total_nrs - rupees) * 100 + 0.5);
+    //     int rs = usd * 137.33 + ce / 100 * 137.;
+    //     return NRS(rupees, paisa);
+    // }
+};
+
+int main()
+{
+    NRS n1(500, 50);
+    USD u1;
+    // Destination = Source
+    u1 = n1;
+    n1.display();
+    u1.display();
+    return 0;
+}
--- files/june 23.cpp
+++ files/june 23.cpp
@@ -1,4 +1,3 @@
-/*
 #include <iostream>
 using namespace std;
 
@@ -57,51 +56,51 @@ int main()
     s3.display();
     return 0;
 }
-*/
 
 
-#include <iostream>
-using namespace std;
 
-class dhewa{
-    private:
-    int rupees, paisa;
-    public:
+// #include <iostream>
+// using namespace std;
+
+// class dhewa{
+//     private:
+//     int rupees, paisa;
+//     public:
     
-    dhewa(){
-        rupees = 0;
-        paisa = 0;
-    }
+//     dhewa(){
+//         rupees = 0;
+//         paisa = 0;
+//     }
 
-    dhewa(int r, int p){
-        rupees = r;
-        paisa = p;
-    }
+//     dhewa(int r, int p){
+//         rupees = r;
+//         paisa = p;
+//     }
 
-    void display(){
-        cout << "I have " << rupees << "rupees" << "and " << paisa << "paisa." << endl;
-    }
+//     void display(){
+//         cout << "I have " << rupees << "rupees" << "and " << paisa << "paisa." << endl;
+//     }
 
-    dhewa operator -=(dhewa d5){
-        dhewa maru;
-        maru.rupees = rupees - d5.rupees;
-        maru.paisa = paisa - d5.paisa;
-        return maru;
-    }
+//     dhewa operator -=(dhewa d5){
+//         dhewa maru;
+//         maru.rupees = rupees - d5.rupees;
+//         maru.paisa = paisa - d5.paisa;
+//         return maru;
+//     }
 
-    // nameless
-    // dhewa operator -= (dhewa d5){
-    //     int r = rupees - d5.rupees;
-    //     int p = paisa - d5.paisa;
-    //     return dhewa(r,p);
-    // }
-};
+//     // nameless
+//     // dhewa operator -= (dhewa d5){
+//     //     int r = rupees - d5.rupees;
+//     //     int p = paisa - d5.paisa;
+//     //     return dhewa(r,p);
+//     // }
+// };
 
-int main(){
-    dhewa d1(150, 75), d2(50, 25), d3;
-    d1.display();
-    d2.display();
-    d3 = d1 -= d2;
-    d3.display();
-    return 0;
-}
+// int main(){
+//     dhewa d1(150, 75), d2(50, 25), d3;
+//     d1.display();
+//     d2.display();
+//     d3 = d1 -= d2;
+//     d3.display();
+//     return 0;
+// }
